2e397f3486fb037af342d29cbaf5491594715165
juzhiyuan <juzhiyuan@apache.org>
2020-08-31 09:15:59 +0800

    feat: improve ASF links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -5,13 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
 class Footer extends React.Component {
   docUrl(doc) {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
     return `${baseUrl}${docsPart}${doc}`;
   }
 
@@ -31,26 +31,26 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>ASF</h5>
-            <a href="https://www.apache.org/security/">Security</a>
             <a href="https://www.apache.org/">Foundation</a>
-            <a href="https://www.apache.org/licenses/">LICENSES</a>
-            {/* <a href="https://apachecon.com/?ref=apisix.apache.org">ApacheCon</a>
-            <a href="https://www.apache.org/foundation/sponsorship.html">Sponsorship</a>
-            <a href="https://www.apache.org/foundation/thanks.html">Thanks</a> */}
+            <a href="https://www.apache.org/licenses/">License</a>
+            <a href="http://www.apache.org/events/current-event/">Events</a>
+            <a href="https://www.apache.org/security/">Security</a>
+            <a href="https://www.apache.org/foundation/sponsorship.html">
+              Sponsorship
+            </a>
+            <a href="https://www.apache.org/foundation/thanks.html">Thanks</a>
           </div>
           <div>
             <h5>Community</h5>
-            <a
-              href="https://github.com/apache/apisix"
-              target="_blank"
-              rel="noreferrer noopener">
-              GitHub
+            <a href="https://github.com/apache/apisix/issues">
+              GitHub Issue Tracker
             </a>
             <a href="https://apisix.slack.com/">Slack</a>
             <a
               href="https://twitter.com/ApacheAPISIX"
               target="_blank"
-              rel="noreferrer noopener">
+              rel="noreferrer noopener"
+            >
               Twitter
             </a>
           </div>
@@ -66,17 +66,17 @@ class Footer extends React.Component {
           target="_blank"
           rel="noreferrer noopener"
           style={{
-            display: 'block',
-            margin: '1em auto',
+            display: "block",
+            margin: "1em auto",
             opacity: 0.8,
-            transition: 'opacity 0.15s ease-in-out',
-            textAlign: 'center'
+            transition: "opacity 0.15s ease-in-out",
+            textAlign: "center",
           }}
         >
           <img
             src={`${this.props.config.baseUrl}img/asf_logo_wide_small.png`}
             alt="Apache Software Foundation"
-            style={{width: 370, height: 64}}
+            style={{ width: 370, height: 64 }}
           />
         </a>
         <section className="copyright">{this.props.config.copyright}</section>
