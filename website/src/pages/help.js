11b7488bae407f49d46964640bd601a20a14af4f
qier222 <qier2222@gmail.com>
2021-02-09 17:37:16 +0800

    refactor: update index page design (#173)
    Co-authored-by: 琚致远 <juzhiyuan@apache.org>

--- website/src/pages/help.js
+++ website/src/pages/help.js
@@ -1,16 +1,16 @@
-const React = require('react');
-import Layout from '@theme/Layout';
+const React = require("react");
+import Layout from "@theme/Layout";
 
 function Help(props) {
-  const { config: siteConfig, language = '' } = props;
+  const { config: siteConfig, language = "" } = props;
   const { baseUrl, docsUrl } = siteConfig;
-  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-  const langPart = `${language ? `${language}/` : ''}`;
+  const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+  const langPart = `${language ? `${language}/` : ""}`;
   const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
   return (
     <div className="container">
       <div className="row">
-        <div className="postHeader">
+        <div className="post-header">
           <h1>Need help?</h1>
         </div>
       </div>
@@ -19,24 +19,24 @@ function Help(props) {
       </div>
       <div className="row">
         <div class="col col--4">
-          <div className="Header">
+          <div className="header">
             <h3>Browse Docs</h3>
           </div>
           <p>
-            Learn more using the{' '}
+            Learn more using the{" "}
             <a href="https://github.com/apache/apisix/tree/master/doc">
-               documentation on this site.
+              documentation on this site.
             </a>
           </p>
         </div>
         <div class="col col--4">
-          <div className="Header">
+          <div className="header">
             <h3>Join the community</h3>
           </div>
           <p>Ask questions about the documentation and project</p>
         </div>
         <div class="col col--4">
-          <div className="Header">
+          <div className="header">
             <h3>Stay up to date</h3>
           </div>
           <p>Find out what's new with this project</p>
