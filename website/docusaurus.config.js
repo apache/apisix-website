03750fcd30f32cc98e7fe6a29aff7b51fa4a08cb
qier222 <qier222@outlook.com>
2021-02-18 11:10:00 +0800

    feat: add events page & improve the Download page (#188)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,268 +6,44 @@ module.exports = {
   baseUrl: "/",
   organizationName: "Apache",
   projectName: "apisix-website",
-  scripts: ["https://buttons.github.io/buttons.js"],
   favicon: "img/favicon.png",
   customFields: {
     tagline2:
       "Apache APISIX software provides rich traffic management features such as load balancing, dynamic upstream, canary release, circuit breaking, authentication, observability, and more.",
-    users: [
-      {
-        caption: "360 奇虎",
-        image: "https://static.apiseven.com/logo/360.png",
-        infoLink: "https://www.360.cn/",
-        pinned: true,
-      },
-      {
-        caption: "大商天狗",
-        image: "https://static.apiseven.com/logo/51tiangou.svg",
-        infoLink: "http://51tiangou.com/",
-        pinned: true,
-      },
-      {
-        caption: "妙笔 AI",
-        image: "https://static.apiseven.com/logo/aimiaobi.png",
-        infoLink: "http://aimiaobi.com/",
-        pinned: true,
-      },
-      {
-        caption: "超链云商",
-        image: "https://static.apiseven.com/logo/chaolian360.png",
-        infoLink: "https://www.chaolian360.com/",
-        pinned: true,
-      },
-      {
-        caption: "DaoCloud",
-        image: "https://static.apiseven.com/logo/daocloud.png",
-        infoLink: "https://www.daocloud.io/",
-        pinned: true,
-      },
-      {
-        caption: "小电科技",
-        image: "https://static.apiseven.com/logo/dian.png",
-        infoLink: "http://dian.so/",
-        pinned: true,
-      },
-      {
-        caption: "嘀嗒出行",
-        image: "https://static.apiseven.com/logo/didachuxing.jpg",
-        infoLink: "https://www.didachuxing.com/",
-        pinned: true,
-      },
-      {
-        caption: "湖南新云网",
-        image: "https://static.apiseven.com/logo/cunw.png",
-        infoLink: "http://www.cunw.com.cn/",
-        pinned: true,
-      },
-      {
-        caption: "大搜车",
-        image: "https://static.apiseven.com/logo/dasouche.png",
-        infoLink: "https://www.dasouche.com/",
-        pinned: true,
-      },
-      {
-        caption: "大淘客",
-        image: "https://static.apiseven.com/logo/dataoke.png",
-        infoLink: "https://www.dataoke.com/",
-        pinned: true,
-      },
-      {
-        caption: "eFactory",
-        image: "https://static.apiseven.com/logo/eFactory.webp",
-        infoLink: "https://www.efactory-project.eu/",
-        pinned: true,
-      },
-      {
-        caption: "理房通",
-        image: "https://static.apiseven.com/logo/ehomepay.png",
-        infoLink: "https://www.ehomepay.com.cn/",
-        pinned: true,
-      },
-      {
-        caption: "简单一点科技",
-        image: "https://static.apiseven.com/logo/eZone.png",
-        infoLink: "https://ezone.work/",
-        pinned: true,
-      },
-      {
-        caption: "fansup",
-        image: "https://static.apiseven.com/logo/fansup.png",
-        infoLink: "https://fansup.mobi/",
-        pinned: true,
-      },
-      {
-        caption: "海尔优家",
-        image: "https://static.apiseven.com/logo/haier.webp",
-        infoLink: "http://haieruplus.com/",
-        pinned: true,
-      },
-      {
-        caption: "中移杭研",
-        image: "https://static.apiseven.com/logo/hy10086.png",
-        infoLink: "https://hy.10086.cn/",
-        pinned: true,
-      },
-      {
-        caption: "航天网信",
-        image: "https://static.apiseven.com/logo/htzhiyun.png",
-        infoLink: "https://www.htzhiyun.cn/",
-        pinned: true,
-      },
-      {
-        caption: "好洛维",
-        image: "https://static.apiseven.com/logo/hellowin.jpg",
-        infoLink: "http://www.hellowin.cn/",
-        pinned: true,
-      },
-      {
-        caption: "HelloTalk",
-        image: "https://static.apiseven.com/logo/HelloTalk.png",
-        infoLink: "https://www.hellotalk.com/",
-        pinned: true,
-      },
-      {
-        caption: "华为",
-        image: "https://static.apiseven.com/logo/huawei.png",
-        infoLink: "http://huawei.com/",
-        pinned: true,
-      },
-      {
-        caption: "虎牙",
-        image: "https://static.apiseven.com/logo/huya.jpeg",
-        infoLink: "https://www.huya.com/",
-        pinned: true,
-      },
-      {
-        caption: "艾佳生活",
-        image: "https://static.apiseven.com/logo/ihomefnt.gif",
-        infoLink: "https://www.ihomefnt.com/",
-        pinned: true,
-      },
-      {
-        caption: "简单心理",
-        image: "https://static.apiseven.com/logo/jiandanxinli.png",
-        infoLink: "https://www.jiandanxinli.com/",
-        pinned: true,
-      },
-      {
-        caption: "同程金服",
-        image: "https://static.apiseven.com/logo/jr_ly.png",
-        infoLink: "https://jr.ly.com/",
-        pinned: true,
-      },
-      {
-        caption: "贝壳找房",
-        image: "https://static.apiseven.com/logo/ke.png",
-        infoLink: "https://www.ke.com/",
-        pinned: true,
-      },
-      {
-        caption: "魅族",
-        image: "https://static.apiseven.com/logo/meizu.jpg",
-        infoLink: "https://www.meizu.com/",
-        pinned: true,
-      },
-      {
-        caption: "明源云客",
-        image: "https://static.apiseven.com/logo/mingyuanyun.jpg",
-        infoLink: "https://www.mingyuanyun.com/",
-        pinned: true,
-      },
-      {
-        caption: "NASA",
-        image: "https://static.apiseven.com/logo/nasa.png",
-        infoLink: "https://www.jpl.nasa.gov/",
-        pinned: true,
-      },
-      {
-        caption: "网易",
-        image: "https://static.apiseven.com/logo/163.png",
-        infoLink: "http://www.163.com/",
-        pinned: true,
-      },
-      {
-        caption: "湖南国科云通",
-        image: "https://static.apiseven.com/logo/sinog2c.png",
-        infoLink: "http://sinog2c.com/",
-        pinned: true,
-      },
-      {
-        caption: "炎黄新星",
-        image: "https://static.apiseven.com/logo/sinovatech.gif",
-        infoLink: "https://www.sinovatech.com/",
-        pinned: true,
-      },
-      {
-        caption: "泰康云",
-        image: "https://static.apiseven.com/logo/taikang.svg",
-        infoLink: "http://taikang.com/",
-        pinned: true,
-      },
-      {
-        caption: "糖豆网",
-        image: "https://static.apiseven.com/logo/tangdou.png",
-        infoLink: "http://www.tangdou.com/",
-        pinned: true,
-      },
-      {
-        caption: "腾讯云",
-        image: "https://static.apiseven.com/logo/TencentCloud.jpg",
-        infoLink: "https://cloud.tencent.com/",
-        pinned: true,
-      },
-
-      {
-        caption: "腾讯优图",
-        image: "https://static.apiseven.com/logo/youtu.jpg",
-        infoLink: "https://open.youtu.qq.com/",
-        pinned: true,
-      },
-      {
-        caption: "万思",
-        image: "https://static.apiseven.com/logo/teamones.png",
-        infoLink: "https://www.teamones.cn/",
-        pinned: true,
-      },
-      {
-        caption: "中国航信",
-        image: "https://static.apiseven.com/logo/TravelSky.png",
-        infoLink: "http://travelsky.com/",
-        pinned: true,
-      },
-      {
-        caption: "随行付",
-        image: "https://static.apiseven.com/logo/vbill.png",
-        infoLink: "https://vbill.cn/",
-        pinned: true,
-      },
-      {
-        caption: "优信二手车",
-        image: "https://static.apiseven.com/logo/xin.png",
-        infoLink: "https://www.xin.com/",
-        pinned: true,
-      },
-      {
-        caption: "雪球",
-        image: "https://static.apiseven.com/logo/xueqiu.jpeg",
-        infoLink: "https://xueqiu.com/",
-        pinned: true,
-      },
-      {
-        caption: "租租车",
-        image: "https://static.apiseven.com/logo/zuzuche.png",
-        infoLink: "https://www.zuzuche.com/",
-        pinned: true,
-      },
-      {
-        caption: "作业帮",
-        image: "https://static.apiseven.com/logo/zybang.png",
-        infoLink: "https://www.zybang.com/",
-        pinned: true,
-      },
-    ],
+    showcases: require("./static/data/showcases.json"),
+    events: require("./static/data/events.json"),
     repoUrl: "https://github.com/apache/apisix",
     docsUrl: "",
+    downloads: [
+      {
+        name: "APISIX™",
+        description: "A dynamic, real-time, high-performance API gateway.",
+        shape: "triangle",
+        color: "var(--ifm-color-primary)",
+        githubRepo: "apache/apisix",
+        version: "2.3",
+        releaseDate: "2021-02-09",
+      },
+      {
+        name: "APISIX™ Dashboard",
+        description:
+          "Designed to make it as easy as possible for users to operate Apache APISIX through a frontend interface.",
+        shape: "square",
+        color: "#10B981",
+        githubRepo: "apache/apisix-dashboard",
+        version: "2.4",
+        releaseDate: "2021-02-12",
+      },
+      {
+        name: "APISIX™ Ingress Controller",
+        description: "An Apache APISIX control plane component.",
+        shape: "hexagon",
+        color: "#2563EB",
+        githubRepo: "apache/apisix-ingress-controller",
+        version: "0.3.0",
+        releaseDate: "2021-02-11",
+      },
+    ],
   },
   onBrokenLinks: "log",
   onBrokenMarkdownLinks: "log",
@@ -293,7 +69,16 @@ module.exports = {
       },
     ],
   ],
-  plugins: [],
+  plugins: [
+    [
+      "@docusaurus/plugin-content-blog",
+      {
+        id: "events",
+        routeBasePath: "events",
+        path: "events",
+      },
+    ],
+  ],
   themeConfig: {
     navbar: {
       title: "Apache APISIX™",
@@ -309,6 +94,11 @@ module.exports = {
         {
           to: "/blog",
           label: "Blog",
+          position: "right",
+        },
+        {
+          to: "/events",
+          label: "Events",
           position: "right",
         },
         {
@@ -380,10 +170,6 @@ module.exports = {
           title: "More",
           items: [
             {
-              label: "User Showcase",
-              to: "https://apisix.apache.org/users/",
-            },
-            {
               label: "Blog",
               to: "https://apisix.apache.org/blog/",
             },
@@ -403,6 +189,16 @@ module.exports = {
       apiKey: "ad95c83c2872f173de8bcc4a0351c5c2",
       indexName: "apache-apisix-website",
     },
+    colorMode: {
+      disableSwitch: true,
+    },
+    metadatas: [
+      {
+        name: "description",
+        content:
+          "Apache APISIX is a dynamic, real-time, high-performance Cloud-Native API gateway, based on the Nginx library and etcd.",
+      },
+    ],
   },
   stylesheets: [
     "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap",
---
+++ website/src/pages/downloads/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import styled from "styled-components";
+import Layout from "@theme/Layout";
+import CodeBlock from "@theme/CodeBlock";
+import ProjectCard from "./ProjectCard";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+const DownloadsPage = styled.div`
+  max-width: var(--ifm-container-width);
+  margin: 0 auto;
+  padding: 2rem var(--ifm-spacing-horizontal);
+  width: 100%;
+`;
+const PageTitle = styled.h1`
+  text-align: center;
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+  font-size: 3rem;
+`;
+const Description = styled.div`
+  margin-top: 6rem;
+
+  h2 {
+    margin-top: 4rem;
+    margin-bottom: 0.8rem;
+  }
+`;
+
+const StyledCodeBlock = styled(CodeBlock)`
+  margin-top: 1rem;
+`;
+
+export default (props) => {
+  const { siteConfig } = useDocusaurusContext();
+  if (!(siteConfig.customFields.downloads || []).length) {
+    return null;
+  }
+  const projects = siteConfig.customFields.downloads.map((project) => {
+    return <ProjectCard {...project} />;
+  });
+
+  return (
+    <Layout>
+      <DownloadsPage>
+        <PageTitle>Downloads</PageTitle>
+        {projects}
+        <Description>
+          <h2>History Versions</h2>
+          <div class="markdown">
+            Find all APISIX releases in the&nbsp;
+            <a href="https://archive.apache.org/dist/apisix/" target="_blank">
+              Archive repository
+            </a>
+            .
+            <br />
+            <a
+              href="https://archive.apache.org/dist/incubator/apisix/"
+              target="_blank"
+            >
+              Incubating Archive repository
+            </a>
+            &nbsp;hosts older releases when APISIX was an incubator project.
+          </div>
+          <h2>Verify the releases</h2>
+          <div class="markdown">
+            <a href="https://downloads.apache.org/apisix/KEYS" target="_blank">
+              Get PGP signatures KEYS
+            </a>
+            <br />
+            It is essential that you verify the integrity of the downloaded
+            files using the PGP or SHA signatures. The PGP signatures can be
+            verified using GPG or PGP. Please download the KEYS as well as the
+            asc signature files for relevant distribution. It is recommended to
+            get these files from the main distribution directory and not from
+            the mirrors.
+            <br />
+            <StyledCodeBlock>{`gpg -i KEYS
+          
+# or
+pgpk -a KEYS
+
+# or
+pgp -ka KEYS`}</StyledCodeBlock>
+            <br />
+            To verify the binaries/sources you can download the relevant asc
+            files for it from main distribution directory and follow the below
+            guide.
+            <StyledCodeBlock>{`gpg --verify apache-apisix-********.asc apache-apisix-********
+
+# or
+pgpv apache-apisix-********.asc
+
+# or
+pgp apache-apisix-********.asc`}</StyledCodeBlock>
+          </div>
+        </Description>
+      </DownloadsPage>
+    </Layout>
+  );
+};
