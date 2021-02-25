098620fb18bee099d36800f89347ecf1ec639078
juzhiyuan <juzhiyuan@users.noreply.github.com>
2021-02-25 06:48:03 +0000

    deploy: 76d37e4e98654881762e4f820d03c02e908d8bd4

---
+++ assets/js/574dc000.3d3106e8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{94:function(e){e.exports=JSON.parse('{"permalink":"/events","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ zh/assets/js/b4a67526.4e6383ea.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{119:function(s){s.exports=JSON.parse('{"pluginId":"docs-apisix-ingress-controller","version":"current","label":"Next","isLast":true,"docsSidebars":{"docs":[]},"permalinkToSidebar":{}}')}}]);
