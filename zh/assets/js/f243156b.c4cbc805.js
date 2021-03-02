9af6f21f0e1823efddcb43fe654140dc7b40233c
juzhiyuan <juzhiyuan@users.noreply.github.com>
2021-03-02 09:17:46 +0000

    deploy: 7d3064da0047bb2c838ae877866c1d3e1b3fa6ef

--- zh/assets/js/f243156b.c4cbc805.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[144],{212:function(s){s.exports=JSON.parse('{"pluginId":"docs-apisix-dashboard","version":"current","label":"Next","isLast":true,"docsSidebars":{"docs":[]},"permalinkToSidebar":{}}')}}]);
