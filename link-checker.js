242cf803b5a10ebd71b3e51dd2247490ddcd126a
bzp2010 <bzp2010@apache.org>
2021-03-20 15:52:49 +0800

    feat: identify and replace non document links

--- link-checker.js
+++ link-checker.js
@@ -20,7 +20,7 @@ const scanFolder = (tarDir) => {
 	return filePaths;
 }
 
-const scanLinkInMDFile = (filePath) => {
+const scanLinkInMDFile = (filePath, project) => {
 	const fileContent = fs.readFileSync(filePath, 'utf-8');
 	const regex = /\[[\s\S]*?\]\([\s\S]*?\)/g;
 	if (fileContent.match(regex)) {
@@ -29,31 +29,57 @@ const scanLinkInMDFile = (filePath) => {
 			const textHrefDivide = item.split('](');
 			const text = textHrefDivide[0].replace('[', '');
 			const url = textHrefDivide[1].replace(')', '');
-			if (!url.endsWith(".md")) return false;
 			return ({url, text, file: filePath});
 		});
 
 		// filter out links to other Markdown files
-		const filteredList = [];
-		const unfilteredList = links.filter((link) => {
-			if (!link) return false;
-			const result = link.url.startsWith("http://") || link.url.startsWith("https://");
-			if (!result) {
-				if (link.url.startsWith("#") || link.url.indexOf("#") > 0) {
-					let split = link.url.split("#");
-					if (split.length > 1) {
-						link.anchor = "#" + split[1];
-						link.url = path.normalize(path.dirname(filePath) + "/" + link.url);
-					} else {
-						link.anchor = link.url;
-						link.url = filePath;
-					}
-				} else {
-					link.url = path.normalize(path.dirname(filePath) + "/" + link.url);
-				}
-				filteredList.push(link)
+		const filteredList = []; // local files
+		const unfilteredList = links.filter((link) => { // web links
+			let url = link.url.trim();
+			if (url.startsWith("http://") || url.startsWith("https://")) {
+				link.url = url;
+				return true;
 			}
-			return result;
+
+			// url preprocess
+			if (url.startsWith("#") || url.indexOf("#") > 0) { // such as "#abcd"
+				let split = url.split("#").filter(item => item !== "");
+				if (split.length > 1) {
+					link.anchor = "#" + split[1];
+					url = path.normalize(path.dirname(filePath) + "/" + link.url);
+				} else {
+					link.anchor = link.url;
+					url = filePath;
+				}
+			} else if (url === "LICENSE" || url === 'logos/apache-apisix.png') {
+				url = "https://github.com/apache/" + project + "/blob/master/" + url;
+			} else if (!url.endsWith(".md")) { // not end with ".md"
+				let lang = filePath.startsWith("website\\docs") ? "en" : filePath.split("i18n\\")[1].split("\\")[0];
+				let subPath = filePath.startsWith("website\\docs") ? path.dirname(filePath.split("docs\\" + project + "\\")[1]) : path.dirname(filePath.split("docs-" + project + "\\current\\")[1]);
+				subPath = subPath !== "." ? subPath + "/" : "";
+				let originPath = path.normalize("docs/" + lang + "/latest/" + subPath + url).replace(/\\/g, '/');
+
+				url = "https://github.com/apache/" + project + "/blob/master/" + originPath;
+			} else { // such as "./abcd", "../abcd", "../../../abcd"
+				url = path.normalize(path.dirname(filePath) + "/" + url);
+			}
+
+			// set url
+			let originLink = link.url;
+			link.url = url;
+
+			// url postprocess
+			if (!url.startsWith("http://") && !url.startsWith("https://")) {
+				filteredList.push(link);
+				return false;
+			}
+
+			// replace the converted link with the original document
+			let documentContent = fs.readFileSync(filePath, 'utf8');
+			documentContent = documentContent.replace(new RegExp(originLink, "g"), link.url);
+			fs.writeFileSync(filePath, documentContent, 'utf8');
+
+			return true;
 		});
 		return {
 			links: unfilteredList,
@@ -96,7 +122,7 @@ const linkValidate = (link) => {
 	childProcess.execSync("npm i --save axios");
 
 	console.log("[Document Scanner] Scan all documents");
-	let allDocPaths = [];
+	let allDocuments = [];
 	projects.map((project) => {
 		let latestDocs = {
 			en: `./website/docs/${project}`,
@@ -105,23 +131,31 @@ const linkValidate = (link) => {
 
 		Object.values(latestDocs).forEach((docPath) => {
 			if (!fs.existsSync(docPath)) return;
-			allDocPaths.push(...scanFolder(docPath))
+			allDocuments.push({
+				files: scanFolder(docPath),
+				docPath,
+				project,
+			})
 		});
 	});
 
 	console.log("[Link Scanner] Scan all links");
 	let externalLinks = []; // links to other sites
 	let internalLinks = []; // links to other Markdown files or anchor
-	for (const file of allDocPaths) {
-		const scanResult = scanLinkInMDFile(file);
-		externalLinks.push(...scanResult.links);
-		internalLinks.push(...scanResult.filteredLinks);
+	for (const documents of allDocuments) {
+		documents.files.forEach((file) => {
+			const scanResult = scanLinkInMDFile(file, documents.project);
+			externalLinks.push(...scanResult.links);
+			internalLinks.push(...scanResult.filteredLinks);
+		});
 	}
 
+	//console.log(`[Link Scanner] External Links\n`, externalLinks.map(item => item.url).join("\n"));
+	//console.log(`[Link Scanner] Internal Links\n`, internalLinks.map(item => item.url).join("\n"));
 	console.log(`[Link Scanner] Scan result: ${externalLinks.length} external links, ${internalLinks.length} internal links`)
 
-	let externalBrokenList = [];
 	console.log(`[Link Checker] Start external link check`);
+	let externalBrokenList = [];
 	let externalLinkCheckPromises = [];
 	externalLinks.forEach((link) => {
 		externalLinkCheckPromises.push(linkValidate(link));
@@ -129,6 +163,7 @@ const linkValidate = (link) => {
 
 	let result = await Promise.all(externalLinkCheckPromises);
 	externalBrokenList.push(...result.filter((item) => item.status !== 200));
+	console.log(`[Link Checker] External link check finished`);
 
 	console.log(`[Link Checker] Start internal link check`);
 	let internalBrokenList = [];
@@ -140,6 +175,7 @@ const linkValidate = (link) => {
 		}
 		console.log(`[Link Checker] check "${link.url}", result is ${exist}`)
 	});
+	console.log(`[Link Checker] Internal link check finished`);
 
 	console.log("[Finish] Write broken list to file");
 	fs.writeFileSync('./brokenLinks.json', JSON.stringify({
