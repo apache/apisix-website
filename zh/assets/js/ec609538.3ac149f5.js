6747dd3fcffcb3b2579188be9ca1fdb6df75eebe
Yiyiyimu <Yiyiyimu@users.noreply.github.com>
2021-06-22 19:54:59 +0000

    deploy: 4b31a461b62f29e46d6f1a1fe0da051c5f9125ca

---
+++ zh/assets/js/ec609538.3ac149f5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[90426],{73001:function(e){"use strict";e.exports=JSON.parse('{"pluginId":"docs-apisix-java-plugin-runner","version":"current","label":"Next","isLast":true,"docsSidebars":{"docs":[]},"permalinkToSidebar":{}}')}}]);
