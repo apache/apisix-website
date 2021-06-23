f4386720721db8ad22800e22f4b955a157ee108c
juzhiyuan <juzhiyuan@apache.org>
2021-06-23 22:26:40 +0800

    feat: support custom branch

--- common.js
+++ common.js
@@ -29,7 +29,7 @@ module.exports = {
     return {
       name: project.name,
       pluginId: `docs-${project.name}`,
-      paths: {
+      latestDocs: {
         en: `./website/docs/${project.name}`,
         zh: `./website/i18n/zh/docusaurus-plugin-content-docs-docs-${project.name}/current`,
       },
