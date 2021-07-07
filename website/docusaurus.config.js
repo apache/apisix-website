a2707085f941d8db54b2b54a93b1f98059671251
spacewander <spacewanderlzx@gmail.com>
2021-07-07 16:18:01 +0800

    fix: miss the "Plugin" in the label

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -378,7 +378,7 @@ module.exports = {
               to: "/docs/java-plugin-runner/development/"
             },
             {
-              label: "Apache™️ APISIX Go Runner",
+              label: "Apache™️ APISIX Go Plugin Runner",
               to: "/docs/go-plugin-runner/getting-started/"
             },
             {
