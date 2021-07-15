ad795c221cca0a82666e41e0494912fd24aa9d99
guoqqqi <979918879@qq.com>
2021-07-15 09:56:19 +0800

    feat: Add home page lead window for 200 contributors  blogs

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -120,7 +120,7 @@ const Showcase = () => {
   }
   const showcases = siteConfig.customFields.showcases.map((user) => (
     <a href={user.infoLink} key={user.infoLink} target="_blank">
-      <img className="user-logo" src={'https://cdn.jsdelivr.net/gh/apache/apisix-website@master/website/static/img/' +  user.image} alt={user.caption} />
+      <img className="user-logo" src={'https://cdn.jsdelivr.net/gh/apache/apisix-website@master/website/static/img/' + user.image} alt={user.caption} />
     </a>
   ));
   const middleIndex = (showcases.length / 2).toFixed(0);
@@ -216,12 +216,12 @@ const ContributionSection = () => {
         <p>Find a good first issue to get you started !</p>
       </div>
       <div className="contribution-link">
-             <Link
-              to="/docs/general/contributor-guide#good-first-issues"
-             >
-              <GitHubLogo className="contribution-logo" />
-              Good First Issues
-            </Link>
+        <Link
+          to="/docs/general/contributor-guide#good-first-issues"
+        >
+          <GitHubLogo className="contribution-logo" />
+          Good First Issues
+        </Link>
       </div>
     </div>
   );
@@ -233,20 +233,20 @@ const NewsletterSection = () => {
     <div className="newsletter">
       <div className="center-elem news-logo">
         <svg className="news-logo-svg" width="185" height="156" viewBox="0 0 185 156" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 155.5L94 0L185 155.5H140L94 83L42.5 155.5H0Z" fill="#F8423F"/>
-        <path d="M94 82.5L42.5 155H0L76.5 57L94 82.5Z" fill="url(#paint0_linear)"/>
-        <path d="M140 155.5H185L94 0L140 155.5Z" fill="url(#paint1_linear)"/>
-        <defs>
-        <linearGradient id="paint0_linear" x1="222.5" y1="50" x2="85" y2="223.5" gradientUnits="userSpaceOnUse">
-        <stop offset="0.536111" stopColor="#FC0A04"/>
-        <stop offset="1" stopColor="#CF0500" stopOpacity="0.77"/>
-        </linearGradient>
-        <linearGradient id="paint1_linear" x1="139.5" y1="1.50861e-06" x2="226" y2="136" gradientUnits="userSpaceOnUse">
-        <stop offset="0.473466" stopColor="#E2423E"/>
-        <stop offset="1" stopColor="#E2423E" stopOpacity="0.77"/>
-        </linearGradient>
-        </defs>
-      </svg>
+          <path d="M0 155.5L94 0L185 155.5H140L94 83L42.5 155.5H0Z" fill="#F8423F" />
+          <path d="M94 82.5L42.5 155H0L76.5 57L94 82.5Z" fill="url(#paint0_linear)" />
+          <path d="M140 155.5H185L94 0L140 155.5Z" fill="url(#paint1_linear)" />
+          <defs>
+            <linearGradient id="paint0_linear" x1="222.5" y1="50" x2="85" y2="223.5" gradientUnits="userSpaceOnUse">
+              <stop offset="0.536111" stopColor="#FC0A04" />
+              <stop offset="1" stopColor="#CF0500" stopOpacity="0.77" />
+            </linearGradient>
+            <linearGradient id="paint1_linear" x1="139.5" y1="1.50861e-06" x2="226" y2="136" gradientUnits="userSpaceOnUse">
+              <stop offset="0.473466" stopColor="#E2423E" />
+              <stop offset="1" stopColor="#E2423E" stopOpacity="0.77" />
+            </linearGradient>
+          </defs>
+        </svg>
       </div>
       <div className="center-elem news-text">
         <h2>Stay up to date about all Apache APISIX™ News</h2>
@@ -258,9 +258,46 @@ const NewsletterSection = () => {
   );
 };
 
+const PicPoster = () => {
+  const [display, setDisplay] = useState(true);
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    addEventListener('click', () => {
+      if (localStorage.getItem('theme') !== theme) {
+        setTheme(localStorage.getItem('theme'));
+      };
+    });
+  }, [theme]);
+
+  if (!display || localStorage.getItem('SHOW_200_CONTRIBUTOR_EVENT_ENTRY')) {
+    return false;
+  };
+
+  const onClose = () => {
+    setDisplay(false);
+    localStorage.setItem('SHOW_200_CONTRIBUTOR_EVENT_ENTRY', 'read');
+  };
+
+  return (
+    <div className="pic-wrapper">
+      <button className="pic-wrapper-close" onClick={onClose}>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+          <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
+        </svg>
+      </button>
+      <a href="/blog/2021/07/06/celebrate-200-contributors" onClick={onClose}>
+        {theme === "light" && <img src="/static/img/pic_poster/200_mark_light.jpg" alt="" />}
+        {theme === "dark" && <img src="/static/img/pic_poster/200_mark_dark.jpg" alt="" />}
+      </a>
+    </div>
+  )
+}
+
 const Index = (props) => {
   return (
     <Layout>
+      <PicPoster />
       <HomeSplash />
       <LearnHow />
       <EventsSection />
