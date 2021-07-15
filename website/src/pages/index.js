b835fb15e47d26af775b75936d72e0ddcf996bfa
guoqqqi <979918879@qq.com>
2021-07-15 23:36:13 +0800

    fix: build CI error

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -261,29 +261,30 @@ const NewsletterSection = () => {
 const PicPoster = () => {
   const [display, setDisplay] = useState(true);
   const [theme, setTheme] = useState('light');
-  const windowGlobal = typeof window !== 'undefined' && window
 
   useEffect(() => {
     addEventListener('click', () => {
-      if (windowGlobal.localStorage.getItem('theme') !== theme) {
+      if (localStorage.getItem('theme') !== theme) {
         setTheme(localStorage.getItem('theme'));
       };
     });
   }, [theme]);
 
-  if (!display || windowGlobal.localStorage.getItem('SHOW_200_CONTRIBUTOR_EVENT_ENTRY')) {
+  if (!display || (typeof window !== 'undefined' && localStorage.getItem('SHOW_200_CONTRIBUTOR_EVENT_ENTRY'))) {
     return false;
-  };
+  }
 
   const onClose = () => {
     setDisplay(false);
-    windowGlobal.localStorage.setItem('SHOW_200_CONTRIBUTOR_EVENT_ENTRY', 'read');
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('SHOW_200_CONTRIBUTOR_EVENT_ENTRY', 'read');
+    }
   };
 
   return (
     <div className="pic-wrapper">
       <button className="pic-wrapper-close" onClick={onClose}>
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" className="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
           <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
         </svg>
       </button>
