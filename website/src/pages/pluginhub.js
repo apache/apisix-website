e9e0d940d3ef71054155036bc8d101800380237a
Kishani Kandasamy <53715187+KishaniKandasamy@users.noreply.github.com>
2021-08-08 09:44:57 +0530

    Update pluginhub.js
    Co-authored-by: 琚致远 <juzhiyuan@apache.org>

--- website/src/pages/pluginhub.js
+++ website/src/pages/pluginhub.js
@@ -128,7 +128,7 @@ function Plugins(props) {
   const { siteConfig } = useDocusaurusContext();
   const sidebar = siteConfig.customFields.plugins.map((section) => {
     return (
-      <Sidebaritem><a className="sidebar-link" href={`#${section.groupName}`}>{section.groupName}</a></Sidebaritem>
+      <Sidebaritem key={xxx}><a className="sidebar-link" href={`#${section.groupName}`}>{section.groupName}</a></Sidebaritem>
     );
   });
   
