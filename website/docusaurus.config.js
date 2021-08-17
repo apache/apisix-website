3f5a1a3948bd9e3db00c739f2d090297083caf51
Baoyuan <baoyuan.top@gmail.com>
2021-08-17 11:10:41 +0800

    fix: change url name

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -463,7 +463,7 @@ module.exports = {
           position: "right",
           items: [
             {
-              to: "/pluginhub",
+              to: "/plugins",
               label: "Plugin Hub",
             },
             {
