85eeb4f71c09337706abd454015e307575f2252a
Shivam Singh <shivam152002@gmail.com>
2021-08-22 04:09:15 +0530

    feat: apisix-website redesign, added asset and config files

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -592,7 +592,7 @@ module.exports = {
     colorMode: {
       defaultMode: 'light',
       disableSwitch: false,
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
     },
     image: 'img/favicon.png',
     metadatas: [
