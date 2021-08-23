b7af48c3506ab7cef5bd0021d3522ef42a052db6
bzp2010 <bzp2010@apache.org>
2021-08-23 10:34:33 +0800

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -219,6 +219,11 @@ module.exports = {
         theme: {
           customCss: "../src/css/customTheme.css",
         },
+        sitemap: {
+          changefreq: 'daily',
+          priority: 0.5,
+          trailingSlash: false,
+        },
       },
     ],
   ],
@@ -371,14 +376,6 @@ module.exports = {
         }) {
           return `https://github.com/apache/apisix-go-plugin-runner/edit/master/docs/en/latest/${docPath}`;
         },
-      },
-    ],
-    [
-      '@docusaurus/plugin-sitemap',
-      {
-        changefreq: 'daily',
-        priority: 0.5,
-        trailingSlash: false,
       },
     ],
   ],
