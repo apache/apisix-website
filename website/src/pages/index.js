07bf37faf2491768c758624746eabefc1c6d544d
liuxiran <belovedxixi@126.com>
2021-09-09 18:01:59 +0800

    fix: remove show case in homepage (#518)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -113,59 +113,6 @@ const LearnHow = () => (
   </div>
 );
 
-const Showcase = () => {
-  const { siteConfig } = useDocusaurusContext();
-  if (!(siteConfig.customFields.showcases || []).length) {
-    return null;
-  }
-  const showcases = siteConfig.customFields.showcases.map((user) => (
-    <a href={user.infoLink} key={user.infoLink} target="_blank">
-      <img className="user-logo" src={'https://cdn.jsdelivr.net/gh/apache/apisix-website@master/website/static/img/' + user.image} alt={user.caption} />
-    </a>
-  ));
-  const middleIndex = (showcases.length / 2).toFixed(0);
-
-  return (
-    <div className="hero text--center showcase">
-      <div className="container">
-        <div className="product-showcase-section">
-          <h1>Who is Using This?</h1>
-        </div>
-        <p>
-          This project is used by all these folks
-          <br />
-          Are you using this project?&nbsp;
-          <a
-            href="https://github.com/apache/apisix/blob/master/powered-by.md"
-            target="_blank"
-            rel="noopener"
-          >
-            <u>Add your company</u>
-          </a>
-        </p>
-        <div className="user-logos">
-          <div className="logo-row">
-            <span className="user-logos-container">
-              <section>
-                <span>{showcases.slice(0, middleIndex)}</span>
-                <span>{showcases.slice(0, middleIndex)}</span>
-              </section>
-            </span>
-          </div>
-          <div className="logo-row">
-            <span className="user-logos-container">
-              <section>
-                <span>{showcases.slice(middleIndex, showcases.length)}</span>
-                <span>{showcases.slice(middleIndex, showcases.length)}</span>
-              </section>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const EventsSection = () => {
   const { siteConfig } = useDocusaurusContext();
   const events = (siteConfig.customFields.events || [])
@@ -264,7 +211,6 @@ const Index = (props) => {
       <HomeSplash />
       <LearnHow />
       <EventsSection />
-      <Showcase />
       <ContributionSection />
       <NewsletterSection />
     </Layout>
