59a754bac8d079cc73a903aa0059d0681a1baad6
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2021-10-05 05:09:44 +0530

    feat: apisix-website redesign, add redesigned-site sections and styles (sub-pr3) (#617)

--- website/src/pages/sections/components/eventPosterCard.jsx
+++ website/src/pages/sections/components/eventPosterCard.jsx
@@ -1,57 +1,58 @@
 import React, { useState, useRef, useEffect } from "react";
 import gsap from "gsap"
+
 import "../../../css/customTheme.css";
 
 const EventPosterCard = () => {
-    const [display, setDisplay] = useState(true);
-    const picRef = useRef(null);
+  const [display, setDisplay] = useState(true);
+  const picRef = useRef(null);
 
-    useEffect(() => {
-        gsap.fromTo(picRef.current, {
-            x: 500,
-            opacity: 0
-          }, {
-            x: 0,
-            opacity: 1,
-            delay: 3.0,
-        });
-    }, []);
+  useEffect(() => {
+    gsap.fromTo(picRef.current, {
+      x: 500,
+      opacity: 0
+      }, {
+      x: 0,
+      opacity: 1,
+      delay: 3.0,
+    });
+  }, []);
 
-    useEffect(() => {
-        if (!localStorage.getItem('SHOW_EVENT_ENTRY')) {
-          setDisplay(true);
-        };
-    }, []);
-  
-    const onClose = () => {
-        gsap.to(picRef.current, {
-          x: 500,
-          opacity: 0,
-          onComplete: ()=>{
-            setDisplay(false);
-          }
-        });
-        if (typeof window !== 'undefined') {
-          localStorage.setItem('SHOW_EVENT_ENTRY', 'true');
-        }
+  useEffect(() => {
+    if (!localStorage.getItem('SHOW_EVENT_ENTRY')) {
+      setDisplay(true);
     };
+  }, []);
   
-    if (!display) {
-      return null;
+  const onClose = () => {
+    gsap.to(picRef.current, {
+      x: 500,
+      opacity: 0,
+      onComplete: ()=>{
+      setDisplay(false);
+      }
+    });
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('SHOW_EVENT_ENTRY', 'true');
     }
+  };
   
-    return (
-      <div ref={picRef} className="pic-wrapper">
-        <button className="pic-wrapper-close" onClick={onClose}>
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" className="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
-            <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
-          </svg>
-        </button>
-        <a href="/events/2021/08/21/shanghai-meetup" onClick={onClose}>
-          <img src="/img/meetup-card.jpg" alt="" />
-        </a>
-      </div>
-    )
+  if (!display) {
+    return null;
   }
   
-  export default EventPosterCard;
+  return (
+    <div ref={picRef} className="pic-wrapper">
+    <button className="pic-wrapper-close" onClick={onClose}>
+      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" className="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+      <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
+      </svg>
+    </button>
+    <a href="/events/2021/08/21/shanghai-meetup" onClick={onClose}>
+      <img src="/img/meetup-card.jpg" alt="" />
+    </a>
+    </div>
+  );
+}
+  
+export default EventPosterCard;
