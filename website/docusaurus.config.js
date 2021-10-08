2c517ae1f08248b4c051ad7efd2ac388950f1fb0
liuxiran <belovedxixi@126.com>
2021-10-08 17:07:29 +0800

    docs: release dashboard 2.9.0

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -34,8 +34,8 @@ module.exports = {
         shape: "square",
         color: "#10B981",
         githubRepo: "apache/apisix-dashboard",
-        version: "2.8",
-        releaseDate: "2021-09-07",
+        version: "2.9.0",
+        releaseDate: "2021-10-08",
         firstDocPath: "/USER_GUIDE",
       },
       {
@@ -128,9 +128,9 @@ module.exports = {
         color: "#10B981",
         githubRepo: "apache/apisix-dashboard",
         githubBranch: "master",
-        downloadPath: "apisix/dashboard/2.8.0/apache-apisix-dashboard-2.8.0-src",
-        version: "2.8",
-        releaseDate: "2021-09-07",
+        downloadPath: "apisix/dashboard/2.9.0/apache-apisix-dashboard-2.9.0-src",
+        version: "2.9.0",
+        releaseDate: "2021-10-08",
         firstDocPath: "/USER_GUIDE",
       },
       {
