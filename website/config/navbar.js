3f9cc8e40c4e379c48698ed4970030b036877cae
琚致远 <juzhiyuan@apache.org>
2021-11-18 23:37:08 +0800

    chore: adjust docusaurus.config.js (#742)

---
+++ website/config/navbar.js
@@ -0,0 +1,113 @@
+module.exports = [
+  {
+    label: "Docs",
+    position: "right",
+    to: "/docs",
+    items: [
+      {
+        label: "Apache APISIX®️",
+        to: "/docs/apisix/getting-started",
+      },
+      {
+        label: "Apache APISIX®️ Dashboard",
+        to: "/docs/dashboard/USER_GUIDE",
+      },
+      {
+        label: "Apache APISIX®️ Ingress Controller",
+        to: "/docs/ingress-controller/getting-started/",
+      },
+      {
+        label: "Apache APISIX®️ Helm Charts",
+        to: "/docs/helm-chart/apisix/",
+      },
+      {
+        label: "Apache APISIX®️ Docker",
+        to: "/docs/docker/build/",
+      },
+      {
+        label: "Apache APISIX®️ Java Plugin Runner",
+        to: "/docs/java-plugin-runner/development/"
+      },
+      {
+        label: "Apache APISIX®️ Go Plugin Runner",
+        to: "/docs/go-plugin-runner/getting-started/"
+      },
+      {
+        label: "Apache APISIX®️ Python Plugin Runner",
+        to: "/docs/python-plugin-runner/getting-started/"
+      },
+      {
+        label: "General",
+        to: "/docs/general/security",
+      },
+    ],
+  },
+  {
+    label: "FAQ",
+    position: "right",
+    items: [
+      {
+        label: "Apache APISIX®",
+        to: "/docs/apisix/FAQ/",
+      },
+      {
+        label: "Apache APISIX® Dashboard",
+        to: "/docs/dashboard/FAQ/",
+      },
+      {
+        label: "Apache APISIX® Ingress Controller",
+        to: "/docs/ingress-controller/FAQ/",
+      },
+      {
+        label: "Apache APISIX® Helm Chart",
+        to: "/docs/helm-chart/FAQ/",
+      },
+    ]
+  },
+  {
+    to: "/blog",
+    label: "Blog",
+    position: "right",
+  },
+  {
+    to: "/downloads",
+    label: "Downloads",
+    position: "right",
+  },
+  {
+    to: "/team",
+    label: "Team",
+    position: "right",
+  },
+  {
+    label: "Resources",
+    position: "right",
+    items: [
+      {
+        to: "/docs/general/community",
+        label: "Community"
+      },
+      {
+        to: "/plugins",
+        label: "Plugin Hub",
+      },
+      {
+        to: "/releases",
+        label: "Releases",
+      },
+      {
+        to: "/help",
+        label: "Help",
+      },
+    ]
+  },
+  {
+    to: "/showcase",
+    label: "Showcase",
+    position: "right",
+  },
+  {
+    type: "localeDropdown",
+    position: "right",
+  },
+]
