44117008564483a8c9c95cab6a583fc60024bc50
oil欧呦 <2228586315@qq.com>
2022-02-17 18:07:16 +0800

    fix: add window judgement

--- website/detectLang.js
+++ website/detectLang.js
@@ -1,9 +1,11 @@
-const browserLang =
-  (navigator.languages && navigator.languages[0]) ||
-  navigator.language ||
-  navigator.userLanguage;
-const currentLang = window.location.pathname.substring(1, 3);
-if (browserLang === "zh-CN" && currentLang !== "zh") {
-  const url = window.location.origin + "/zh" + window.location.pathname;
-  window.location.replace(url);
+if (typeof window !== "undefined") {
+  const browserLang =
+    (window.navigator.languages && window.navigator.languages[0]) ||
+    window.navigator.language ||
+    window.navigator.userLanguage;
+  const currentLang = window.location.pathname.substring(1, 3);
+  if (browserLang === "zh-CN" && currentLang !== "zh") {
+    const url = window.location.origin + "/zh" + window.location.pathname;
+    window.location.replace(url);
+  }
 }
