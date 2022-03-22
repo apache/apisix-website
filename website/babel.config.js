12914097ab3df16c0a1984244621de07687ef477
SkyeYoung <iskyex@outlook.com>
2022-03-22 12:50:05 +0800

    fix: warnings of babel-plugin-styled-components (#966)

--- website/babel.config.js
+++ website/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
   plugins: ["babel-plugin-styled-components"],
+  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
 };
