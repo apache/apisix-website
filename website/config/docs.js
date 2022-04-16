bf9bdb2e4d77aae0c3cd9e5690cb211792a03e69
tzssangglass <tzssangglass@gmail.com>
2022-04-16 13:23:11 +0800

    feat: release APISIX 2.13.1 (#1036)

--- website/config/docs.js
+++ website/config/docs.js
@@ -6,8 +6,8 @@ module.exports = [
     shape: 'triangle',
     color: '#e8433e',
     githubRepo: 'apache/apisix',
-    version: '2.13.0',
-    releaseDate: '2022-03-24',
+    version: '2.13.1',
+    releaseDate: '2022-04-15',
     firstDocPath: '/getting-started',
   },
   {
