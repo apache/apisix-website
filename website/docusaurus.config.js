2313426bab058c964614b7c60486f6e640c90853
Young <iskyex@outlook.com>
2022-05-12 13:56:44 +0800

    fix: rm trailing slash (#1083)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,6 +14,7 @@ module.exports = {
     'Apache APISIX is a dynamic, real-time, high-performance Cloud-Native API gateway, based on the Nginx library and etcd.',
   url: 'https://apisix.apache.org',
   baseUrl: '/',
+  trailingSlash: false,
   organizationName: 'Apache',
   projectName: 'apisix-website',
   favicon: 'img/favicon.png',
