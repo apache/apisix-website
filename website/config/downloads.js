b2440604f54af831207a42d87b67b39403e9d210
白泽平 <bzp2010@apache.org>
2022-06-06 09:09:27 +0800

    feat: bump dashboard to 2.13.0

--- website/config/downloads.js
+++ website/config/downloads.js
@@ -26,8 +26,8 @@ module.exports = [
     githubBranch: 'master',
     downloadPath: 'apisix/dashboard/2.11.0/apache-apisix-dashboard-2.11.0-src',
     dockerhubPath: 'apisix-dashboard',
-    version: '2.11.0',
-    releaseDate: '2022-03-23',
+    version: '2.13.0',
+    releaseDate: '2022-06-01',
     firstDocPath: '/USER_GUIDE',
   },
   {
