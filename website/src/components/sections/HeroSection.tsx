e29a3666b620953c56db973e4dd7ad5a28777cbb
Young <iskyex@outlook.com>
2022-06-29 03:52:15 +0000

    fix: landing page render logic

--- website/src/components/sections/HeroSection.tsx
+++ website/src/components/sections/HeroSection.tsx
@@ -1,70 +1,51 @@
 import type { FC } from 'react';
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import gsap from 'gsap';
 import Translate from '@docusaurus/Translate';
 
 import '../../css/customTheme.css';
-import HeroCanvas from '../HeroCanvas';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import ArrowAnim from '../ArrowAnim';
 
-const HeroSection: FC = () => {
-  const titleRef = useRef<HTMLHeadingElement>();
-  const subtitleRef = useRef<HTMLHeadingElement>();
-  const ctaRef = useRef<HTMLHeadingElement>();
-  const canRef = useRef<HTMLHeadingElement>();
+const LazyLoadHeroCanvas = () => (
+  <BrowserOnly>
+    {() => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+      const HeroCanvas = require('../HeroCanvas').default;
+      return <HeroCanvas />;
+    }}
+  </BrowserOnly>
+);
 
-  useEffect(() => {
-    const tl = gsap.timeline({
-      defaults: {
-        ease: 'Expo.easeInOut',
-      },
-    });
-
-    tl.fromTo([titleRef.current, subtitleRef.current, ctaRef.current], {
-      opacity: 0,
-      y: 10,
-    }, {
-      opacity: 1,
-      y: 0,
-      stagger: 0.3,
-    });
-
-    return () => {
-      tl.pause(0).kill();
-    };
-  }, []);
-
-  return (
-    <div className="hero-sec-wrap" style={{ width: '100%' }}>
-      <div className="hero-text">
-        <h1 ref={titleRef} className="hero-title hide-title">
-          <span><Translate id="hero.component.title.fragment1">Full Lifecycle API Management</Translate></span>
-          {' '}
-          <br />
-          <span style={{ color: '#E8433E', fontSize: 32 }}>
-            <Translate id="hero.component.title.fragment2">API Gateway, Ingress Controller, etc.</Translate>
-          </span>
-        </h1>
-        <h2 ref={subtitleRef} className="hero-subtitle hide-subtitle">
-          <Translate id="hero.component.subtitle.content">Apache APISIX provides rich traffic management features like Load Balancing, Dynamic Upstream, Canary Release, Circuit Breaking, Authentication, Observability, etc.</Translate>
-        </h2>
-        <div ref={ctaRef} className="hero-ctas hide-ctas">
-          <Link
-            to={useBaseUrl('downloads')}
-            className="btn btn-download"
-          >
-            <Translate id="hero.component.download.btn">Downloads</Translate>
-          </Link>
-          <ArrowAnim />
-        </div>
-      </div>
-      <div ref={canRef} className="add-margin">
-        <HeroCanvas />
+const HeroSection: FC = () => (
+  <div className="hero-sec-wrap" style={{ width: '100%' }}>
+    <div className="hero-text">
+      <h1 className="hero-title">
+        <span><Translate id="hero.component.title.fragment1">Full Lifecycle API Management</Translate></span>
+        {' '}
+        <br />
+        <span style={{ color: '#E8433E', fontSize: 32 }}>
+          <Translate id="hero.component.title.fragment2">API Gateway, Ingress Controller, etc.</Translate>
+        </span>
+      </h1>
+      <h2 className="hero-subtitle">
+        <Translate id="hero.component.subtitle.content">Apache APISIX provides rich traffic management features like Load Balancing, Dynamic Upstream, Canary Release, Circuit Breaking, Authentication, Observability, etc.</Translate>
+      </h2>
+      <div className="hero-ctas">
+        <Link
+          to={useBaseUrl('downloads')}
+          className="btn btn-download"
+        >
+          <Translate id="hero.component.download.btn">Downloads</Translate>
+        </Link>
+        <ArrowAnim />
       </div>
     </div>
-  );
-};
+    <div className="add-margin">
+      <LazyLoadHeroCanvas />
+    </div>
+  </div>
+);
 
 export default HeroSection;
