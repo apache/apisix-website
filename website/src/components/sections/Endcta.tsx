7de129e2b620a659cd6510b854e1f4df025a2cdf
Young <iskyex@outlook.com>
2022-07-03 10:18:17 +0800

    refactor: endcta (#1177)

--- website/src/components/sections/Endcta.tsx
+++ website/src/components/sections/Endcta.tsx
@@ -2,31 +2,33 @@ import type { FC } from 'react';
 import React from 'react';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-
 import Translate from '@docusaurus/Translate';
-
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import useWindowType from '@theme/hooks/useWindowSize';
 import ArrowAnim from '../ArrowAnim';
+import style from '../../css/endcta.module.scss';
 
-const EndCTA: FC = () => (
-  <div className="endcta" style={{ padding: '50px 0', background: '#FF90A3' }}>
-    <div className="endcta-text">
-      <p style={{
-        display: 'flex', justifyContent: 'center', alignItems: 'center', whiteSpace: 'pre',
-      }}
-      >
-        Try
-        <span style={{ color: '#E8433E' }}>
+const EndCTA: FC = () => {
+  const windowType = useWindowType();
+
+  return (
+    <div className={style.endcta}>
+      <p className={style.text}>
+        <span>
+          Try
           {' '}
+        </span>
+        <span className={style.apisix}>
           Apache APISIX
           {' '}
         </span>
-        today
-        <LazyLoadImage className="rocket" src="https://static.apiseven.com/202202/rocket.gif" alt="Rocket" width="6vw" height="6vw" />
+        <span>
+          today.
+        </span>
+        {windowType === 'desktop'
+         && (<LazyLoadImage className={style.rocket} src="https://static.apiseven.com/202202/rocket.gif" alt="Rocket" width="6vw" height="6vw" />)}
       </p>
-    </div>
-    <div className="endcta-btns">
-      <div className="hero-ctas">
+      <div className={style.links}>
         <Link
           to={useBaseUrl('downloads')}
           className="btn btn-download"
@@ -36,7 +38,7 @@ const EndCTA: FC = () => (
         <ArrowAnim />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default EndCTA;
