32e6ac2794708d2ddf048036f3f0ac450a541419
SkyeYoung <iskyex@outlook.com>
2022-07-19 12:09:13 +0800

    fix: sitemap script

--- scripts/update-sitemap-loc.js
+++ scripts/update-sitemap-loc.js
@@ -1,32 +1,54 @@
-/**
- * This script updates the sitemap.xml file with the correct location
- * See https://github.com/apache/apisix-website/issues/705
-*/
+const { js2xml, xml2js } = require('xml-js');
+const { stat, readFile, writeFile } = require('node:fs/promises');
+const Listr = require('listr');
 
-const convert = require('xml-js');
-const fs = require('fs');
+const sitemapXMLs = [
+  ['../website/build/sitemap.xml', '../blog/en/build/sitemap.xml'],
+  ['../website/build/zh/sitemap.xml', '../blog/zh/build/sitemap.xml'],
+];
 
-const SITEMAP_XML_PATH = '../website/build/sitemap.xml';
-try {
-  if (!fs.existsSync(SITEMAP_XML_PATH)) {
-    throw new Error(`${SITEMAP_XML_PATH} does not exist`);
-  }
+const tasks = new Listr([
+  {
+    title: `Check sitemap.xml files exist`,
+    task: () => Promise.all(
+      sitemapXMLs
+        .flat()
+        .map((f) => stat(f).then((stat) => (stat.isFile()
+          ? Promise.resolve()
+          : Promise.reject(new Error(`${f} is not a file`))))),
+    ),
+  },
+  {
+    title: `Merge sitemap.xml files`,
+    task: () => new Listr(
+      sitemapXMLs.map((group) => ({
+        title: `Merge ${group[0]}`,
+        task: () => Promise.all(
+          group.map((f) => readFile(f, 'utf8').then((xml) => xml2js(xml, { compact: true }))),
+        )
+          .then((sitemaps) => {
+            const res = sitemaps[0];
+            for (let i = 1; i < sitemaps.length; i += 1) {
+              res.urlset.url = [
+                ...res.urlset.url,
+                ...sitemaps[i].urlset.url,
+              ];
+            }
+            return res;
+          })
+          .then((sitemap) => writeFile(group[0], js2xml(sitemap, { compact: true }, 'utf-8'))),
+      })),
+      { concurrent: sitemapXMLs.length },
+    ),
+  },
+]);
 
-  const xml = fs.readFileSync(SITEMAP_XML_PATH, 'utf8');
-  const result = JSON.parse(convert.xml2json(xml, { compact: true }));
-  result.urlset.url = result.urlset.url.map((item) => {
-    // eslint-disable-next-line no-underscore-dangle
-    const targetLoc = item.loc._text.endsWith('/') ? item.loc._text : `${item.loc._text}/`;
-    return {
-      loc: {
-        _text: targetLoc,
-      },
-      changefreq: item.changefreq,
-      priority: item.priority,
-    };
+tasks
+  .run()
+  .then(() => {
+    console.log(`[Finish] Generate sitemap.xml`);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
   });
-  fs.writeFileSync(SITEMAP_XML_PATH, convert.json2xml(result, { compact: true, spaces: 4 }), 'utf8');
-  console.log(`Updated ${SITEMAP_XML_PATH} successfully`);
-} catch (error) {
-  console.warn(error);
-}
