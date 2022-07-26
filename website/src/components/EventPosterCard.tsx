c9a700961e0e135fbbeda6d1c950807d97a94c26
SkyeYoung <iskyex@outlook.com>
2022-07-26 17:48:34 +0800

    feat: EventPosterCard adapted to mobile

--- website/src/components/EventPosterCard.tsx
+++ website/src/components/EventPosterCard.tsx
@@ -1,12 +1,11 @@
 import type { FC } from 'react';
-import React, {
-  useCallback, useMemo, useEffect,
-} from 'react';
+import React, { useCallback, useMemo, useEffect } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { useSpring, animated } from 'react-spring';
 import useSessionStorage from 'react-use/lib/useSessionStorage';
-import config from '../../../config/event-poster-card.json';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import config from 'apisix-website/config/event-poster-card.json';
 import style from '../css/event-poster-card.module.scss';
 
 interface EventPosterCardInfo {
@@ -19,10 +18,15 @@ interface EventPosterCardInfo {
 
 const SHOW_STORE_KEY = 'SHOW_EVENT_ENTRY';
 
-const EventPosterCard:FC<Omit<EventPosterCardInfo, 'show' | 'expire'>> = (props) => {
+const EventPosterCard: FC<Omit<EventPosterCardInfo, 'show' | 'expire'>> = (props) => {
   const { image, links, width } = props;
-  const { i18n: { currentLocale } } = useDocusaurusContext();
-  const link = useMemo(() => (typeof links === 'string' ? links : links[currentLocale]), [currentLocale]);
+  const {
+    i18n: { currentLocale },
+  } = useDocusaurusContext();
+  const link = useMemo(
+    () => (typeof links === 'string' ? links : links[currentLocale]),
+    [currentLocale],
+  );
   const [, setStoreShow] = useSessionStorage(SHOW_STORE_KEY, 'true');
 
   const [styles, api] = useSpring(() => ({
@@ -43,29 +47,29 @@ const EventPosterCard:FC<Omit<EventPosterCardInfo, 'show' | 'expire'>> = (props)
   }, []);
 
   const onClose = useCallback(
-    async () => Promise.all(api.start({
-      to: {
-        x: 500,
-        opacity: 0,
-      },
-    }))
-      .then(() => setStoreShow('false')),
+    async () => Promise.all(
+      api.start({
+        to: {
+          x: 500,
+          opacity: 0,
+        },
+      }),
+    ).then(() => setStoreShow('false')),
     [api],
   );
 
   return (
     <animated.div className={style.picWrapper} style={styles}>
       <button className={style.closeBtn} onClick={onClose} type="button">
-        <svg
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 352 512"
-        >
-          <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+        <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+          <path
+            fill="currentColor"
+            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+          />
         </svg>
       </button>
       <a href={link} onClick={onClose} target="_blank" rel="noreferrer">
-        <LazyLoadImage src={image} alt={link} width={width} />
+        <LazyLoadImage src={image} alt={link} width={width} style={{ maxWidth: '100vw' }} />
       </a>
     </animated.div>
   );
@@ -76,7 +80,7 @@ const EventPosterCardWrapper: FC = () => {
   const { show, expire, ...rest } = config;
   const expireTimestamp = new Date(expire).getTime();
 
-  if (show && !storeShow && (expireTimestamp > Date.now())) {
+  if (show && !storeShow && expireTimestamp > Date.now()) {
     return <EventPosterCard {...rest} />;
   }
 
