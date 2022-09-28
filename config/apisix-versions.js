824e93aba1305445a90e6fa6a21be3f622bc3356
罗泽轩 <spacewanderlzx@gmail.com>
2022-09-28 14:26:43 +0800

    fix: update version list (#1348)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- config/apisix-versions.js
+++ config/apisix-versions.js
@@ -3,7 +3,7 @@
 /**
  * @type {Array<string>} version list
  */
-const versions = ['2.12', '2.13', '2.14', '2.15'];
+const versions = ['2.12', '2.13', '2.14', '2.15', '2.99'];
 
 /**
  * @type {Array<string>} LTS version list
