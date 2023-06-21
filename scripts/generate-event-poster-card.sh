0329db15a14a39f36d1f3a0fd7c709d9bb09d44c
Qi Guo <979918879@qq.com>
2023-06-21 11:00:33 +0800

    fix: remove doc event poster (#1621)

--- scripts/generate-event-poster-card.sh
+++ scripts/generate-event-poster-card.sh
@@ -6,5 +6,4 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")/..
 
 cp ${SCRIPTPATH}/config/event-poster-card.json ${SCRIPTPATH}/website/src/theme/Footer/
-cp ${SCRIPTPATH}/website/src/theme/Footer/* ${SCRIPTPATH}/doc/src/theme/Footer/
 cp ${SCRIPTPATH}/website/src/theme/Footer/* ${SCRIPTPATH}/blog/src/theme/Footer/
