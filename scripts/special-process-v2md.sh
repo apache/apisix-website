c29c338f5448d18959416831f7ddea4d2609632e
SkyeYoung <iskyex@outlook.com>
2023-10-31 18:35:29 +0800

    fix(scripts): solve ingress documentt mdx render

--- scripts/special-process-v2md.sh
+++ scripts/special-process-v2md.sh
@@ -6,3 +6,5 @@
 BASEDIR=$(dirname $0)/..
 
 sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs/apisix-ingress-controller/references/v2.mdx
+sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs-apisix-ingress-controller_versioned_docs/version-1.7.0/references/v2.mdx
+sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs-apisix-ingress-controller_versioned_docs/version-1.7.0-release/references/v2.mdx
