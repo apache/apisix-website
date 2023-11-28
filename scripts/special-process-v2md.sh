cfc766326f907a0e28b8855dc33d53ee827545fe
Young <iskyex@outlook.com>
2023-11-28 20:59:16 +0800

    feat: improve compatibility with macOS (#1727)

--- scripts/special-process-v2md.sh
+++ scripts/special-process-v2md.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # remove <!--\s*markdown-link-check-disable\s*--> and <!--\s*markdown-link-check-enable\s*-->
 # in /apisix-ingress-controller/references/v2.mdx
 # after synced docs
 
 BASEDIR=$(dirname $0)/..
 
-sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs/apisix-ingress-controller/references/v2.mdx
-sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs-apisix-ingress-controller_versioned_docs/version-1.7.0/references/v2.mdx
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs/apisix-ingress-controller/references/v2.mdx
+  sed -i '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs-apisix-ingress-controller_versioned_docs/version-1.7.0/references/v2.mdx
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs/apisix-ingress-controller/references/v2.mdx
+  sed -i '' '/<!--\s*markdown-link-check-disable\s*-->/I,+1d; /<!--\s*markdown-link-check-enable\s*-->/I,+1d;' $BASEDIR/doc/docs-apisix-ingress-controller_versioned_docs/version-1.7.0/references/v2.mdx
+else
+  echo "Unsupported OS: $OSTYPE"
+  exit 1
+fi
