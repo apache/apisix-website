d059e2443deb20345792471ee84263c550a320fc
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-10-17 11:02:18 +0530

    docs: release APISIX 3.14.1 (#1960)

--- config/docs.js
+++ config/docs.js
@@ -6,8 +6,8 @@ module.exports = [
     shape: 'triangle',
     color: '#e8433e',
     githubRepo: 'apache/apisix',
-    version: '3.14.0',
-    releaseDate: '2025-10-10',
+    version: '3.14.1',
+    releaseDate: '2025-10-17',
     firstDocPath: '/getting-started',
   },
   {
