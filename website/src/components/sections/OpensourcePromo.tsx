63ae47c43056f028bd1cf542bb5c99cecd520563
Ming Wen <moonbingbing@gmail.com>
2026-06-26 07:34:47 +0800

    fix(website): drop the dark OSS-promo card, center the dev resources
    Remove the "Building API Gateway Together" dark card from the open-source
    promo and present "Learn from developers" as a single centered block with
    a width-capped, responsive (fluid) video.

--- website/src/components/sections/OpensourcePromo.tsx
+++ website/src/components/sections/OpensourcePromo.tsx
@@ -6,7 +6,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Translate from '@docusaurus/Translate';
 
 import BrowserOnly from '@docusaurus/BrowserOnly';
-import GitHub from '../../assets/icons/github-logo.svg';
 import type { VideoProps } from '../Video';
 import style from '../../css/landing-sections/os-promo.module.scss';
 
@@ -53,30 +52,6 @@ const LazyLoadVideo = () => (
 
 const OpensourcePromo: FC = () => (
   <div className={style.osPromotion}>
-    <div className={style.ossPromo}>
-      <h3>
-        <Translate id="openSourcePromo.component.ossPromo.title">
-          Building API Gateway Together
-        </Translate>
-      </h3>
-      <div className={style.ossPromoSubtitle}>
-        <p>
-          <Translate id="openSourcePromo.component.ossPromo.subtitle1">
-            Apache APISIX is open source and ever-growing.
-          </Translate>
-          <br />
-          <Translate id="openSourcePromo.component.ossPromo.subtitle2">
-            Contributors are always welcome, reach out to us on GitHub.
-          </Translate>
-        </p>
-      </div>
-      <div className={style.ossCta}>
-        <GitHub style={{ width: '20px', margin: '0 10px 0 0' }} />
-        <a href="https://github.com/apache/apisix">
-          <Translate id="openSourcePromo.component.link.Github">Check us out</Translate>
-        </a>
-      </div>
-    </div>
     <div className={style.docsPromo}>
       <div className={style.docsPromoHead}>
         <h3>
@@ -106,7 +81,7 @@ const OpensourcePromo: FC = () => (
           </p>
         </div>
       </div>
-      <div className="docs-promo-video">
+      <div className={style.docsPromoVideo}>
         <LazyLoadVideo />
       </div>
     </div>
